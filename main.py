874640bf2b10242e1c381ead95818bfd14cdb200
msugiii <banirunn@gmail.com>
2024-09-08 16:22:57 +0900

    fix models

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-import base64
-from typing import Union
-from fastapi.responses import JSONResponse
 
 import controller
 
+
 class Item(BaseModel):
     image: str
+
 
 class Output(BaseModel):
     image: str
     description: str
     object_names: list[str]
 
+
 class Data(BaseModel):
     data: Output
+
 
 app = FastAPI()
 
@@ -23,4 +24,8 @@ app = FastAPI()
 @app.post("/")
 def input(item: Item) -> Data:
     situation, base64_string, object_names = controller.run(item.image)
-    return Data(image=base64_string, description=situation, object_names=object_names)
+    return Data(
+        data=Output(
+            image=base64_string, description=situation, object_names=object_names
+        )
+    )
